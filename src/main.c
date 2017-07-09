70e67f60900c42741fafed60e9ee358c1c562e6c
schdck <mauricio.schmaedeck@gmail.com>
2017-07-09 03:42:09 -0300

    Adicionar instruções para compiar e executar na pasta main

--- src/main.c
+++ src/main.c
@@ -10,7 +10,18 @@
 #include "../include/allegro_utils.h"
 #include "../include/menu_functions.h"
 
-// g++ -o ../app *.c -I/usr/include/allegro5 -L/usr/lib -lallegro -lallegro_image -lallegro_dialog -lallegro_font -lallegro_ttf -lallegro_primitives
+/*
+
+Para compilar, navegar até a pasta 'src' e utilizar o comando:
+
+g++ -o ../app *.c -I/usr/include/allegro5 -L/usr/lib -lallegro -lallegro_image -lallegro_dialog -lallegro_font -lallegro_ttf -lallegro_primitives
+
+Para executar, navegar até a pasta 'src' e utilizar o comand:
+
+../app
+
+*/
+
 
 int main(int argc, char **argv)
 {  
